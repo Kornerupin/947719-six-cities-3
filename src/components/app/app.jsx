dea5f7c7bfe20eef64847cc14d58d0f8d5a2f43d
Ilya Krupin <dev@krupibox.ru>
2020-02-05 15:22:11 +0300

    Add 1-4 tasks

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../main/main.jsx";
+
+const App = () => {
+  // eslint-disable-next-line react/prop-types
+
+  return <Main />;
+};
+
+export default App;
