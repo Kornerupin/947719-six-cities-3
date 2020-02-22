b85749bdb0563375a5fa31504368d129c4d5fc6b
Ilya Krupin <dev@krupibox.ru>
2020-02-22 10:44:41 +0300

    Update e2e test for OfferList

--- src/components/offer-list/offer-list.e2e.test.js
+++ src/components/offer-list/offer-list.e2e.test.js
@@ -1,20 +1,20 @@
 import OfferList from './offer-list';
 import offers from '../../mocks/offers';
 
-const card = offers;
-
-it(`It expect an Array that contains the following properties`, () => {
+it(`It expect an Object that contains the following properties`, () => {
   shallow(<OfferList
     offers={offers}
   />);
 
-  expect(card).toEqual(
-      expect.arrayContaining({
-        image: expect.arrayContaining([expect.any(String)]),
-        type: expect.any(String),
-        price: expect.any(String),
-        name: expect.any(String)
-      })
-  );
-
+  offers.map((offer) => {
+    expect(offer).toEqual(
+        expect.objectContaining({
+          image: expect.arrayContaining([expect.any(String)]),
+          type: expect.any(String),
+          price: expect.any(String),
+          name: expect.any(String)
+        })
+    );
+  });
 });
+
