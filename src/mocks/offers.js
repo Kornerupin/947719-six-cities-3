44d357b42ff8e159cd4720c4a7c209fecfcf36bd
Ilya Krupin <dev@krupibox.ru>
2020-02-27 10:33:53 +0300

    Add leaflet and coordinates to mock

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -8,27 +8,31 @@ export default [
     type: `Apartment`,
     price: `80`,
     rating: 3.1,
-    name: `Beautiful &amp; luxurious apartment at great location`
+    name: `Beautiful &amp; luxurious apartment at great location`,
+    marker: [52.3909553943508, 4.85309666406198]
   },
   {
     image: [`img/room.jpg`],
     type: `Private room`,
     price: `132`,
     rating: 1.8,
-    name: `Wood and stone place`
+    name: `Wood and stone place`,
+    marker: [52.369553943508, 4.85309666406198]
   },
   {
     image: [`img/apartment-02.jpg`],
     type: `Apartment`,
     price: `180`,
     rating: 3.8,
-    name: `Canal View Prinsengracht`
+    name: `Canal View Prinsengracht`,
+    marker: [52.3909553943508, 4.929309666406198]
   },
   {
     image: [`img/apartment-03.jpg`],
     type: `Apartment`,
     price: `80`,
     rating: 4.5,
-    name: `Nice, cozy, warm big bed apartment`
+    name: `Nice, cozy, warm big bed apartment`,
+    marker: [52.3809553943508, 4.939309666406198]
   },
 ];
