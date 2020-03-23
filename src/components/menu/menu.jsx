9fad9ec19bbc849f9c97afff09bac61458596ff7
Ilya Krupin <dev@krupibox.ru>
2020-03-23 11:38:56 +0300

    Fix teamlead recomendations

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,23 +1,20 @@
 const Menu = ({cities, onCityClick}) => {
 
-  cities = cities ? cities.map((city, index) => {
-
-    return (
-      <li key={index.toString()} className="locations__item">
-        <a className="locations__item-link tabs__item" href="#" onClick={() => {
-          onCityClick(city);
-        }}>
-          <span>{city}</span>
-        </a>
-      </li>
-    );
-  }).slice(0, 6) : false;
-
   return (
     <ul className="locations__list tabs__list">
       {
-        cities
-      }
+        cities.slice(0, 6).map((city, index) =>
+          <li key={`menu-${index}`} className="locations__item">
+            <a className="locations__item-link tabs__item" href="#"
+              onClick={
+                () => {
+                  onCityClick(city);
+                }
+              }>
+              <span>{city}</span>
+            </a>
+          </li>
+        )}
     </ul>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
 const MIN_RATING = 20;
 
 export const getStars = (rating) => Math.round(rating) * MIN_RATING;
+
+export const getFormatDate = (utc) => {
+  const date = new Date(utc);
+
+  return `${date.toLocaleString(`default`, {month: `long`})} ${date.getDate()}, ${date.getFullYear()}`;
+};
