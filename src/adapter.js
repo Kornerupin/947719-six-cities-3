603279c94b791baac476efd01c7b4143e2d07658
Ilya Krupin <dev@krupibox.ru>
2020-03-26 15:31:30 +0300

    Update data

--- src/adapter.js
+++ src/adapter.js
@@ -1,56 +1,53 @@
 class Adapter {
-    constructor(data) {
-        this.id = data.id;
-        this.city = data.city.name;
-        this.cityCenter = [data.city.location.latitude, data.city.location.longitude];
-        this.cityZoom = data.city.location.zoom;
-        this.images = data.images;
-        this.image = data.preview_image;
-        this.name = data.title;
-        this.isFavorite = data.is_favorite;
-        this.isPremium = data.is_premium;
-        this.rating = data.rating;
-        this.type = data.type;
-        this.bedrooms = data.bedrooms;
-        this.max_adults = data.max_adults;
-        this.price = data.price;
-        this.goods = data.goods;
-        this.coordinates = [data.location.latitude, data.location.longitude];
-        this.description = data.description;
-        this.reviews = [
-          {
-            id: data.host.id,
-            avatar: data.host.avatar_url,
-            name: data.host.name,
-            is_pro: data.host.is_pro,
-            rating: 4.1,
-            text: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
-            time: 1569006474313
-          },
-          {
-            id: ++data.host.id,
-            avatar: `https://api.adorable.io/avatars/128/0.33908914729720996`,
-            name: `Wax`,
-            is_pro: data.host.is_pro,
-            rating: 4.5,
-            text: `It is my favorite hotel. I have been staying her for several years and can confirm that service level remains at the same high level.`,
-            time: 1571598485610
-          },
-          {
-            id: ++data.host.id,
-            avatar: `https://api.adorable.io/avatars/128/0.06997580364137201`,
-            name: `Rax`,
-            is_pro: data.host.is_pro,
-            rating: 3.8,
-            text: `Emplacement parfait, beaucoup de commerces à proximité, le tout dans un lieu traditionnel et agréable, j'ai adoré mon court séjour et aimerait beaucoup y retourner un jour!`,
-            time: 1574276894250
-          }
-        ]
-    }
-
-    static parse(data) {
-      return new Adapter(data);
-    }
+  constructor(data) {
+    this.id = data.id;
+    this.city = data.city.name;
+    this.cityCenter = [data.city.location.latitude, data.city.location.longitude];
+    this.cityZoom = data.city.location.zoom;
+    this.images = data.images.slice(0, 6);
+    this.image = data.preview_image;
+    this.name = data.title;
+    this.isFavorite = data.is_favorite;
+    this.isPremium = data.is_premium;
+    this.rating = data.rating;
+    this.type = `${data.type[0].toUpperCase()}${data.type.slice(1)}`;
+    this.bedrooms = data.bedrooms;
+    this.capacity = data.max_adults;
+    this.price = data.price;
+    this.facilities = data.goods;
+    this.coordinates = [data.location.latitude, data.location.longitude];
+    this.hostAvatar = data.host.avatar_url;
+    this.hostName = data.host.name;
+    this.hostMessage = data.description;
+    this.hostIsPro = data.host.is_pro,
+    this.reviews = [
+      {
+        avatar: `https://api.adorable.io/avatars/128/0.9912918678423135`,
+        name: `Max`,
+        rating: 4.1,
+        text: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+        time: 1569006474313
+      },
+      {
+        avatar: `https://api.adorable.io/avatars/128/0.33908914729720996`,
+        name: `Wax`,
+        rating: 4.5,
+        text: `It is my favorite hotel. I have been staying her for several years and can confirm that service level remains at the same high level.`,
+        time: 1571598485610
+      },
+      {
+        avatar: `https://api.adorable.io/avatars/128/0.06997580364137201`,
+        name: `Rax`,
+        rating: 3.8,
+        text: `Emplacement parfait, beaucoup de commerces à proximité, le tout dans un lieu traditionnel et agréable, j'ai adoré mon court séjour et aimerait beaucoup y retourner un jour!`,
+        time: 1574276894250
+      }
+    ]
   }
-  
-  export default Adapter;
+
+  static parse(data) {
+    return new Adapter(data);
+  }
+}
+
+export default Adapter;
--- src/components/offer-detail/offer-detail.jsx
+++ src/components/offer-detail/offer-detail.jsx
@@ -8,15 +8,12 @@ const sortReviews = (reviews) => reviews.slice().sort((a, b) => b.time - a.time)
 
 const OfferDetail = (props) => {
   const { offer, offers, onOfferNameClick, currentCoordinate } = props;
-  const { type, price, rating, name, reviews, cityCenter, cityZoom } = offer;
+  const { images, type, bedrooms, capacity, price, rating, name, facilities, hostAvatar, hostName, hostMessage, hostIsPro, reviews, cityCenter, cityZoom, isPremium } = offer;
   const OFFERS_MAX = 3;
   const REVIEWS_MAX = 10;
 
-  console.log(offer);
-  
-  
   const neighbors = offers.slice().filter((element) => element.coordinates !== currentCoordinate);
-      
+
   return (
     <div className="page">
       <header className="header">
@@ -45,31 +42,20 @@ const OfferDetail = (props) => {
         <section className="property">
           <div className="property__gallery-container container">
             <div className="property__gallery">
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/room.jpg" alt="Photo studio" />
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio" />
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio" />
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio" />
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/studio-01.jpg" alt="Photo studio" />
-              </div>
-              <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio" />
-              </div>
+              {
+                images.map((image, index) =>
+                  <div key={`image-${index}`} className="property__image-wrapper">
+                    <img className="property__image" src={image} alt="Photo studio" />
+                  </div>
+                )
+              }
             </div>
           </div>
           <div className="property__container container">
             <div className="property__wrapper">
-              <div className="property__mark">
+              {isPremium && <div className="property__mark">
                 <span>Premium</span>
-              </div>
+              </div>}
               <div className="property__name-wrapper">
                 <h1 className="property__name">
                   {name}
@@ -93,10 +79,11 @@ const OfferDetail = (props) => {
                   {type}
                 </li>
                 <li className="property__feature property__feature--bedrooms">
-                  3 Bedrooms
+                  {bedrooms} Bedrooms
+
                 </li>
                 <li className="property__feature property__feature--adults">
-                  Max 4 adults
+                  Max {capacity} adults
                 </li>
               </ul>
               <div className="property__price">
@@ -106,54 +93,28 @@ const OfferDetail = (props) => {
               <div className="property__inside">
                 <h2 className="property__inside-title">What&apos;s inside</h2>
                 <ul className="property__inside-list">
-                  <li className="property__inside-item">
-                    Wi-Fi
-                  </li>
-                  <li className="property__inside-item">
-                    Washing machine
-                  </li>
-                  <li className="property__inside-item">
-                    Towels
-                  </li>
-                  <li className="property__inside-item">
-                    Heating
-                  </li>
-                  <li className="property__inside-item">
-                    Coffee machine
-                  </li>
-                  <li className="property__inside-item">
-                    Baby seat
-                  </li>
-                  <li className="property__inside-item">
-                    Kitchen
-                  </li>
-                  <li className="property__inside-item">
-                    Dishwasher
-                  </li>
-                  <li className="property__inside-item">
-                    Cabel TV
-                  </li>
-                  <li className="property__inside-item">
-                    Fridge
-                  </li>
+                  {
+                    facilities.map((facility, index) =>
+                      <li key={`facility-${index}`} className="property__inside-item">
+                        {facility}
+                      </li>
+                    )
+                  }
                 </ul>
               </div>
               <div className="property__host">
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
-                  <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" alt="Host avatar" width={74} height={74} />
+                  <div className={`property__avatar-wrapper ${hostIsPro && `property__avatar-wrapper--pro`} user__avatar-wrapper`}>
+                    <img className="property__avatar user__avatar" src={hostAvatar} alt="Host avatar" width={74} height={74} />
                   </div>
                   <span className="property__user-name">
-                    Angelina
+                    {hostName}
                   </span>
                 </div>
                 <div className="property__description">
                   <p className="property__text">
-                    A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                  </p>
-                  <p className="property__text">
-                    An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
+                    {hostMessage}
                   </p>
                 </div>
               </div>
